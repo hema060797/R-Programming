422e1887c756bce32235c94f8b01e9756f7074bc
Hema <53147814+hema060797@users.noreply.github.com>
2021-02-05 02:12:08 -0800

    Add files via upload

---
+++ test.R
@@ -0,0 +1,58 @@
+myString <- "Hello, World!"
+
+print ( myString)
+
+myString1 <- "hi"
+print( myString1)
+
+
+# multi-line comment
+if(FALSE) {
+  "This is a demo for multi-line comments and it should be put inside either a 
+      single OR double quote"
+}
+
+myString <- "Hello, World!"
+print ( myString)
+
+
+#Datatype 
+#Logical 
+v <- TRUE
+print(class(v))
+
+#Numeric
+v1 <- 23.5
+print(class(v1))
+
+#Integer
+v2 <- 10L
+print(class(v2))
+
+#character
+v4 <- 'hello'
+#v4 <- "hello"
+#v4 <- '23'
+print(class(v4))
+
+#complex
+v5 <- 2 + 5i
+print(class(v5))
+
+#raw 
+v6 <- charToRaw("hello")
+print(class(v6))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
